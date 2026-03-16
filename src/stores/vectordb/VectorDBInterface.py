242288f259d401c2037c52af5ef0e603f465caf8
Omneya Zayed <72omnia@gmail.com>
2026-03-17 00:54:02 +0200

    created an interface to be implemented by vectordbs

---
+++ src/stores/vectordb/VectorDBInterface.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+class VectorDBInterface(ABC):
+
+    @abstractmethod
+    def connect(self):
+        pass
+
+    @abstractmethod
+    def disconnect(self):
+        pass
+
+    @abstractmethod
+    def does_collection_exist(self, collection_name: str) -> bool:
+        pass
+
+    @abstractmethod
+    def list_all_collections(self) -> List:
+        pass
+
+    @abstractmethod
+    def get_collection_info(self, collection_name: str) -> dict:
+        pass
+
+    @abstractmethod
+    def create_collection(self, collection_name: str,
+                          embedding_size: int,
+                          do_reset: bool):
+        pass
+
+    @abstractmethod
+    def delete_collection(self, collection_name: str) -> None:
+        pass
+
+    @abstractmethod
+    def insert_one(self, collection_name: str, text: str,
+                   vector: list, metadata: dict=None, record_id: str=None):
+        pass
+
+    @abstractmethod
+    def insert_many(self, collection_name: str, texts: list, vectors: list, 
+                    metadata: list, record_ids: list, batch_size: int=50):
+        pass
+
+    @abstractmethod
+    def search_by_vector(self, collection_name: str, vector: list, limit: int):
+        pass
