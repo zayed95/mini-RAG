d91b7a1adcdd314406d0c0f5e1c5e10647eb0b3b
Omneya Zayed <72omnia@gmail.com>
2026-04-05 23:47:36 +0200

    more enumerations for semantic search

--- src/models/enums/ResponseEnum.py
+++ src/models/enums/ResponseEnum.py
@@ -10,4 +10,9 @@ class ResponseSignal(Enum):
     PROCESSING_SUCCESS = 'processing_succeeded'
     NO_FILES_ERROR = 'no_files_found'
     FILE_ID_ERROR='no_file_found_with_that_id'
-    
+    PROJECT_NOT_FOUND_ERROR = "project_not_found"
+    INSERT_INTO_VECTOR_DB_ERROR = "could_not_insert_into_vectordb"
+    INSERT_INTO_VECTOR_DB_SUCCESS = "insert_into_vectordb_success"
+    VECTORDB_COLLECTION_RETRIEVED = "vectordb_collection_retrieved"
+    VECTORDB_SEARCH_ERROR = "vectordb_search_error"
+    VECTORDB_SEARCH_SUCCESS = "vectordb_search_success"
